074cbba98b2bbf35012eefa2aa8d32e51979f513
Tihomir Stoychev Stoychev <tihomir_alcudia3@hotmail.com>
2019-01-23 08:36:24 +0100

    jorge nos ha hackiado

--- PruebasBurbuja/UnitTest1.vb
+++ PruebasBurbuja/UnitTest1.vb
@@ -5,8 +5,23 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 <TestClass()> Public Class UnitTest1
     Dim lista As List(Of Integer) = New List(Of Integer) From {2, 3, 4, 53, 1}
     Dim vector As VectorA = New VectorA(lista)
-    <TestMethod()> Public Sub ComprobarForPrueba()
 
+    <TestMethod()> Public Sub ComprobarForPrueba()
+        Dim aux As List(Of Integer) = lista
+        aux.Sort()
+        Assert.AreEqual(vector.burbujaFor(), aux)
+    End Sub
+
+    <TestMethod()> Public Sub ComprobarWhilePrueba()
+        Dim aux As List(Of Integer) = lista
+        aux.Sort()
+        Assert.AreEqual(vector.burbujaFor(), aux)
+    End Sub
+
+    <TestMethod()> Public Sub ComprobarDoWhilePrueba()
+        Dim aux As List(Of Integer) = lista
+        aux.Sort()
+        Assert.AreEqual(vector.burbujaDoWhile(), aux)
     End Sub
 
 End Class
